d5ce0345c5c601b63f0d2b6b3feeae8d2f526ab0
Chaitanya Mhatre <cmh24292@gmail.com>
2020-11-18 13:48:43 +0530

    basic downloading

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,1 +1,28 @@
-youtube_downloader
+# Youtube Video Downloader
+
+from pytube import YouTube
+import time
+
+SAVE_PATH = "E:/"
+
+LINK = "" # Copy youtube video Link here
+
+try:
+	yt = YouTube(LINK)
+except:
+	print('Conncetion Error')
+
+
+# Video Length
+len_in_seconds = yt.length - 1
+original_len = time.strftime("%H:%M:%S", time.gmtime(len_in_seconds)) 
+print(original_len)
+
+# Video Dowloading 
+
+try:
+    stream = yt.streams.first()
+    stream.download(SAVE_PATH) 
+except: 
+    print("Download Failed !!") 
+print('Task Completed!')
